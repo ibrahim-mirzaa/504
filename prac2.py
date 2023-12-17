81d1ccc62a0736c175ab88722a2c9e266b7cfcd4
Ibrahim Mirza <117998485+ibrahim-mirzaa@users.noreply.github.com>
2023-12-17 22:05:25 +0530

    Add files via upload

---
+++ prac2.py
@@ -0,0 +1,14 @@
+#learn basic game designing technique
+import pygame
+pygame.init()
+screen = pygame.display.set_mode([500, 500])
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    screen.fill((255, 255, 255))
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    pygame.display.flip()
+pygame.quit()
+
